6680870117eaffd2131380bac647468fbeab7d04
Vladislav <vlad.pavlov24@gmail.com>
2015-02-25 18:22:13 +0300

    Painter is correct. TL is got due to long creation of the array

--- F_Painter/src/MainProgramF.java
+++ F_Painter/src/MainProgramF.java
@@ -3,13 +3,15 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+
+import com.sun.javafx.scene.traversal.TopMostTraversalEngine;
 
 
 public class MainProgramF{
 
+	
 	private static class TreeElement{
 		//
 		public int value;
@@ -27,6 +29,9 @@ public class MainProgramF{
 			this.buffer = -1;
 		}
 	}
+
+	public static final int BLACK = 0;
+	public static final int WHITE = 2;
 	
 
 	private static class SegmentTreeLazy{
@@ -36,38 +41,43 @@ public class MainProgramF{
 		private TreeElement[] treeElements;
 		//
 		public SegmentTreeLazy(int[] result){
+			
 			actualElementAmount = result.length;
-			Integer twoPowerInf = Integer.highestOneBit(actualElementAmount);
-			Integer leafLineLength = ((actualElementAmount & (actualElementAmount - 1)) == 0) ? twoPowerInf : (twoPowerInf << 1) ;
+			int twoPowerInf = Integer.highestOneBit(actualElementAmount);
+			int leafLineLength = ((actualElementAmount & (actualElementAmount - 1)) == 0) ? twoPowerInf : (twoPowerInf << 1) ;
 			int totalCapacity = (leafLineLength << 1) - 1;
 			treeElements = new TreeElement[totalCapacity];
-			leafLineStart = totalCapacity - leafLineLength; 
+			System.out.println(totalCapacity);
+			leafLineStart = totalCapacity - leafLineLength;
+			long g = System.currentTimeMillis();
 			for (int i = totalCapacity - 1; i >= 0; i--){
 				if (i >= totalCapacity - (leafLineLength - actualElementAmount)){
-					treeElements[i] = new TreeElement(2, i - leafLineStart, i - leafLineStart);	
+					treeElements[i] = new TreeElement(2, i - leafLineStart, i - leafLineStart);
 				}
 				else if (i >= totalCapacity - leafLineLength){
-					treeElements[i] = new TreeElement(result[i - leafLineStart], i - leafLineStart ,i - leafLineStart);
+					treeElements[i] = new TreeElement(result[i - leafLineStart], i - leafLineStart , i - leafLineStart);
 				}
 				else{
-					int newVal = sum(treeElements[2 * i + 1], treeElements[2 * i + 2]);
+					int newVal = sum(treeElements[2 * i + 1].value, treeElements[2 * i + 2].value);
 					int newLeft = treeElements[2 * i + 1].leftControl;
 					int newRight = treeElements[2 * i + 2].rightControl;
 					treeElements[i] = new TreeElement(newVal, newLeft, newRight);
 				}
 
 			}
+			System.out.println((System.currentTimeMillis() - g) / 1000.0);
 		}
-		
-
+		/*
 	    public void printBinaryTree(int vertex, int level){
 	        if(vertex >= treeElements.length)
 	             return;
 	        printBinaryTree(2 * vertex + 2, level + 1);
 	        if(level != 0){
-	            for(int i = 0; i < level - 1;i++)
+	            for(int i = 0; i < level - 1; i++){
 	                System.out.print("|\t");
-	                System.out.println("|-------" + treeElements[vertex].value + "(" + treeElements[vertex].buffer + ")" );
+	            }
+	            String index = isLeaf(vertex) ? (" ~ " + treeElements[vertex].leftControl) : ("");
+	            System.out.println("|-------" + treeElements[vertex].value + "(" + treeElements[vertex].buffer + ")"  + index);
 	        }
 	        else
 	            System.out.println(treeElements[vertex].value + "(" + treeElements[vertex].buffer + ")");
@@ -79,16 +89,17 @@ public class MainProgramF{
 			printBinaryTree(0, 0);
 			System.out.print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< \n");
 		}
-		
+		*/
 
 		private boolean isLeaf(int vertexNumber){
 			return treeElements[vertexNumber].leftControl == treeElements[vertexNumber].rightControl;
 		}
 
-		private int sum(TreeElement a, TreeElement b){
-			return (a.value + b.value) / 2;
+		private int sum(int a, int b){
+			return (a == b) ? a : 1;
 		}
-
+		
+		/*
 		public double sumAtSegmentFromUp(int left, int right) throws Exception{
 			//System.out.println(" + Sum query for ["+left + " ; "+ right +"]");
 			if ((left <= right) && (left >= 0) && (right < actualElementAmount)){
@@ -97,28 +108,31 @@ public class MainProgramF{
 				throw new Exception("Incorrect query : [" + left + ";" + right + "]");
 		}
 
-		private double sumAtSegmentRecursive(int vertexNumber, int left, int right){
+		private int sumAtSegmentRecursive(int vertexNumber, int left, int right){
 			TreeElement current = treeElements[vertexNumber];
+			//the needed segment
 			if ((current.leftControl == left) && (current.rightControl == right)){
 				if (current.buffered){
-					return sum(treeElements[2 * vertexNumber + 1], treeElements[2 * vertexNumber + 2]);
+					return current.buffer; //in normal case buffer * len
 				}else{
 					return current.value;
 				}
 			}else{
 				pushBufferDeeper(vertexNumber);
-				double leftSum = 0;
-				double rightSum = 0;
-				if (left <= treeElements[2 * vertexNumber + 1].rightControl){
-					leftSum = sumAtSegmentRecursive(2 * vertexNumber + 1, left, Math.min(treeElements[2*vertexNumber+1].rightControl, right));
+				int leftSum = 0;
+				int rightSum = 0;
+				int leftSon = 2 * vertexNumber+ 1; 
+				int rightSon = 2 * vertexNumber+ 2;
+				if (left <= treeElements[leftSon].rightControl){
+					leftSum = sumAtSegmentRecursive(leftSon, left, Math.min(treeElements[leftSon].rightControl, right));
 				}
 				if (right >= treeElements[2 * vertexNumber + 2].leftControl){
-					rightSum = sumAtSegmentRecursive(2 * vertexNumber + 2, Math.max(treeElements[2 * vertexNumber + 2].leftControl, left), right);
+					rightSum = sumAtSegmentRecursive(rightSon, Math.max(treeElements[rightSon].leftControl, left), right);
 				}
-				return (leftSum + rightSum) / 2.0; 
+				return sum(leftSum, rightSum); 
 			}
 		}
-		
+		*/
 		public void updateSegmentLazy(int leftBound, int rightBound, int newValue){
 			updateSegmentLazyRecursive(0, leftBound, rightBound, newValue);
 		}
@@ -128,24 +142,24 @@ public class MainProgramF{
 			//look if current node is the target segment
 			if ((current.leftControl == leftBound) && (current.rightControl == rightBound)){
 				//here we use lazy approach
-				if (isLeaf(vertex)){
-					//no need to buffer, update straight
+				if (!isLeaf(vertex)){
+					//set the buffer that will be pushed down later if needed! SUPER LAZY!
+					current.buffered = true;
+					current.buffer = newValue;
+					//update the value to hold correct information 
 					current.value = newValue;
 					return current.value;
 				}else{
-					//update the value, set the buffer that will be pushed later! LAZY!
-					System.out.println(vertex + " is now buffered");
-					current.buffered = true;
-					current.buffer = newValue;
+					//no buffer possible here, update the value without troubles
 					current.value = newValue;
 					return current.value;
 				}
-			//need to go deeper!
-			}else{
-				//To save some old changes push them down, updating values. 
+			}else{//oh no, need to go deeper!
+				//To save old changes push them down, updating values there(*). 
 				pushBufferDeeper(vertex);
-				int leftSon = 2 * vertex + 1;
+				int leftSon = 2 * vertex + 1; 
 				int rightSon = 2 * vertex + 2;
+				//Now there are valid values(*), so we can account them.
 				int newLeft = treeElements[leftSon].value;
 				int newRight = treeElements[rightSon].value;
 				// Look if left son needs to be updated if it intersects with target segment.
@@ -156,7 +170,7 @@ public class MainProgramF{
 				if (treeElements[rightSon].leftControl <= rightBound){
 					newRight = updateSegmentLazyRecursive(rightSon, Math.max(treeElements[rightSon].leftControl, leftBound), rightBound, newValue);
 				}
-				current.value = (newLeft + newRight) / 2;
+				current.value = sum(newLeft, newRight);
 				return current.value;
 			}
 		}
@@ -164,7 +178,7 @@ public class MainProgramF{
 		private void pushBufferDeeper(int vertex){
 			TreeElement current = treeElements[vertex];
 			if (current.buffered){
-				System.out.println("Pushing deeper from " + vertex);
+				//System.out.println("Pushing deeper from " + vertex);
 				int leftSon = 2 * vertex + 1;
 				int rightSon = 2 * vertex + 2;
 				//if children are leaves - no need to buffer them, set the values from parent buffer
@@ -172,11 +186,11 @@ public class MainProgramF{
 					treeElements[leftSon].value = current.buffer;
 					treeElements[rightSon].value = current.buffer;	
 				}else{
-					//push changes to left son and set the correct value here
+					//push changes to left son and set the actual value from the buffer 
 					treeElements[leftSon].buffered = true;
 					treeElements[leftSon].buffer = current.buffer;
 					treeElements[leftSon].value = current.buffer;
-					// buffer the right son, set the correct value now
+					// buffer the right son, set the actual value from the buffer
 					treeElements[rightSon].buffered = true;
 					treeElements[rightSon].buffer = current.buffer;
 					treeElements[rightSon].value = current.buffer;
@@ -186,8 +200,8 @@ public class MainProgramF{
 				current.buffer = -1;	
 			}
 		}
-		//	
-
+			
+/*
 		public void finalizeUpdates() {
 			finalizeUpdatesRecusively(0);
 		}
@@ -209,11 +223,36 @@ public class MainProgramF{
 				finalizeUpdatesRecusively(2 * vertex + 2);
 			}
 		}
+*/
+		public int[] calculateBlackSegments() {
+			int[] result = new int[3];
+			result[2] = Integer.MIN_VALUE;
+			return updateResultRecursively(0, result);
+		}
+
+		private int[] updateResultRecursively(int vertex, int[] result) {
+			TreeElement current = treeElements[vertex];
+			if (current.value == BLACK){
+				if (current.leftControl != result[2] + 1){ // if it is not a continuing
+					result[0]++;
+				}
+				result[1] += current.rightControl - current.leftControl + 1;
+				result[2] = current.rightControl; // move the right border
+			}else if(!isLeaf(vertex) && current.value != WHITE){
+				updateResultRecursively(2*vertex + 1, result);
+				updateResultRecursively(2*vertex + 2, result);
+			}
+			return result;
+		}
+		
 	}
 
-	private static String inputFileName = "painter.in";
+	private static String inputFileName = "painter2.in";
 	private static String outputFileName = "painter.out";
+	
+	
 	private static long startMoment = 0L;
+	
 	private static void checkTime(boolean start){
 		if (start){
 			startMoment = System.currentTimeMillis();
@@ -222,61 +261,48 @@ public class MainProgramF{
 		}
 	}
 	
-	/*
-	public static int[] nextArray(int size, BufferedReader reader) throws IOException{
-		int[] array = new int[size];
-		String numbers = reader.readLine();
-		int firstDigit = 0;
-		int whitespaceIndex = 0;
-		for (int i = 0; i < size; i++){
-			whitespaceIndex = numbers.indexOf(' ', firstDigit);
-			whitespaceIndex = (whitespaceIndex == -1) ? numbers.length() : whitespaceIndex;
-			int number = int.parseint(numbers.substring(firstDigit, whitespaceIndex));
-			array[i] = number;
-			firstDigit = whitespaceIndex + 1;
-		}
-		return array;
-	}
-	*/
-
 	
 	public static void main(String[] args) throws Exception{
-		checkTime(true);
+		int[] result = new int[3];
 		File input = new File(inputFileName);
 		File output = new File(outputFileName);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(input)), 32768);
 		BufferedWriter writer = new BufferedWriter(new FileWriter(output));
-		Integer queryAmount = Integer.parseInt(reader.readLine());
+		int queryAmount = Integer.parseInt(reader.readLine());
 		System.out.println("Processing " + queryAmount + " queries");
-		int size = 10;
-		int array[] = new int[size];
-		Arrays.fill(array, 2);
-		SegmentTreeLazy tree = new SegmentTreeLazy(array);
-		tree.printTree();
+		int halfSize = 500000;
+		int line[] = new int[halfSize + 1 + halfSize];
+		Arrays.fill(line, WHITE);
+		System.out.println("Creation of the tree");
+			checkTime(true);
+		SegmentTreeLazy tree = new SegmentTreeLazy(line);
+			checkTime(false);
+		//tree.printTree();
+		System.out.println("The loop");checkTime(true);
 		for (int k = 0; k < queryAmount; k++){
 			String query = reader.readLine();
+			//System.out.println(query);
 			String[] parameters = query.split(" ");
 			int color = parameters[0].equals("W") ? 2 : 0;
-			int leftBound = Integer.parseInt(parameters[1]);
-			int rightBound = leftBound + Integer.parseInt(parameters[2]);
-			System.out.println("Updating [" + leftBound + ";" + rightBound  + "] to" + color);
+			int leftBound = Integer.parseInt(parameters[1]) + halfSize;
+			int rightBound = leftBound + Integer.parseInt(parameters[2]) - 1;
+			//System.out.println("Updating [" + leftBound + ";" + rightBound  + "] to " + color);
 			tree.updateSegmentLazy(leftBound, rightBound, color);
-			tree.printTree();
-		}
-		tree.finalizeUpdates();
-		System.out.println("FINAL ARRAY IS READY.");
-		/*
-		for (int offset = 0; offset < elementAmount; offset++){
-			if (offset == elementAmount - 1){
-				writer.write((tree.treeElements[tree.leafLineStart + offset].value) + "");
+			//tree.printTree();
+			result = tree.calculateBlackSegments();
+			if (k == queryAmount - 1){
+				writer.write(result[0] + " " + result[1]);
 			}else{
-				writer.write(tree.treeElements[tree.leafLineStart + offset].value + " ");
+				writer.write(result[0] + " " + result[1] + "\n");	
 			}
+			//System.out.println(result[0] + " " + result[1]);
 		}
-		*/
+		checkTime(false);
+		//tree.finalizeUpdates();
+		//System.out.println("Answer is ready.");
+		//tree.printTree();
 		reader.close();
 		writer.close();
-		checkTime(false);
 		System.out.println("Finished.");
 	}
 
